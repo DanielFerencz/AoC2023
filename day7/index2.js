3c501e86480f00407b9dba8f15b5c97ac1baae6b
ferencz.daniel <ferencz.daniel@codespring.ro>
2023-12-07 10:17:10 +0200

    day7 done

---
+++ day7/index2.js
@@ -0,0 +1,151 @@
+const { readFile } = require("fs/promises");
+
+cards = {
+    2: 1,
+    3: 2,
+    4: 3,
+    5: 4,
+    6: 5,
+    7: 6,
+    8: 7,
+    9: 8,
+    T: 9,
+    J: 0,
+    Q: 11,
+    K: 12,
+    A: 13,
+};
+
+const higherFirst = (a, b) => {
+    for (let i = 0; i < 5; i++) {
+        if (cards[a.at(i)] > cards[b.at(i)]) {
+            return 1;
+        } else {
+            if (cards[a.at(i)] < cards[b.at(i)]) {
+                return -1;
+            }
+        }
+    }
+
+    return 0;
+};
+
+const whatKind = (a) => {
+    const cardss = {};
+
+    for (let i = 0; i < 5; i++) {
+        if (cardss[a.at(i)] === undefined) {
+            cardss[a.at(i)] = 1;
+        } else {
+            cardss[a.at(i)] += 1;
+        }
+    }
+
+    let strength = 0;
+
+    let jokers = cardss.J | 0;
+
+    if (jokers !== 0) {
+        if (jokers === 5) return 10;
+        if (jokers === 4) return 10;
+
+        let maxi = 0;
+        let maxiV;
+
+        for (let key of Object.keys(cardss)) {
+            if (key !== "J") {
+                if (maxi < cardss[key]) {
+                    maxi = cardss[key];
+                    maxiV = key;
+                }
+            }
+        }
+
+        cardss[maxiV] += jokers;
+
+        for (let val of Object.keys(cardss)) {
+            if (val !== "J") {
+                if (cardss[val] === 5) {
+                    return 10;
+                }
+
+                if (cardss[val] === 4) {
+                    return 9;
+                }
+
+                if (cardss[val] === 3) {
+                    strength += 3;
+                }
+
+                if (cardss[val] === 2) {
+                    strength += 1;
+                }
+            }
+        }
+    } else {
+        for (let val of Object.values(cardss)) {
+            if (val === 5) {
+                return 10;
+            }
+
+            if (val === 4) {
+                return 9;
+            }
+
+            if (val === 3) {
+                strength += 3;
+            }
+
+            if (val === 2) {
+                strength += 1;
+            }
+        }
+    }
+
+    // console.log(jokers, cardss, strength, Object.keys(cardss));
+
+    return strength;
+};
+
+const sorting = (a, b) => {
+    const sA = whatKind(a.card);
+    const sB = whatKind(b.card);
+
+    if (sA > sB) return 1;
+    if (sB > sA) return -1;
+
+    return higherFirst(a.card, b.card);
+};
+
+const main = async () => {
+    const text = await readFile("./input.txt", "utf8");
+
+    // console.log(text);
+
+    const games = text.split("\n");
+
+    let num = 0;
+
+    console.log(games);
+
+    const plays = games.map((a) => {
+        const play = a.split(" ");
+
+        return {
+            card: play[0],
+            bid: parseInt(play[1], 10),
+        };
+    });
+
+    plays.sort((a, b) => sorting(a, b));
+
+    // console.log(plays);
+
+    for (let i = 0; i < plays.length; i++) {
+        num += (i + 1) * plays[i].bid;
+    }
+
+    console.log(num);
+};
+
+main();
