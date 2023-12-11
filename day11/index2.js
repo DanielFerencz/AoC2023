1309e2058dbc8fc35a5a6ceeddce814fff413c11
ferencz.daniel <ferencz.daniel@codespring.ro>
2023-12-11 12:53:20 +0200

    day11 done

--- day11/index2.js
+++ day11/index2.js
@@ -1,16 +1,40 @@
 const { readFile } = require("fs/promises");
 
+const isEmptyRow = (row) => {
+    for (let i = 0; i < row.length; i++) {
+        if (row[i] !== "*") return false;
+    }
+    return true;
+};
+
 const calculateDistances = (mapp) => {
     const stars = [];
     let num = 0;
 
+    let howManyX = 0;
+
     for (let i = 0; i < mapp.length; i++) {
-        for (let j = 0; j < mapp[i].length; j++) {
-            if (mapp[i][j] === "#") {
-                stars.push({ x: i, y: j });
+        let howManyY = 0;
+
+        if (isEmptyRow(mapp[i])) {
+            howManyX += 1;
+        } else {
+            for (let j = 0; j < mapp[i].length; j++) {
+                if (mapp[i][j] === "*") {
+                    howManyY += 1;
+                }
+
+                if (mapp[i][j] === "#") {
+                    stars.push({
+                        x: i + howManyX * 999999,
+                        y: j + howManyY * 999999,
+                    });
+                }
             }
         }
     }
+
+    // console.log(stars);
 
     for (let i = 0; i < stars.length - 1; i++) {
         for (let j = i + 1; j < stars.length; j++) {
@@ -44,31 +68,27 @@ const main = async () => {
         }
 
         if (empty) {
-            mapp.splice(i, 0, Array.from(".".repeat(mapp[i].length)));
-            i++;
+            mapp.splice(i, 1, Array.from("*".repeat(mapp[i].length)));
         }
     }
-
-    console.log(mapp.map((val) => val.join("")));
 
     for (let j = 0; j < mapp[0].length; j++) {
         let empty = true;
 
         for (let i = 0; i < mapp.length && empty; i++) {
-            if (mapp[i][j] !== ".") {
+            if (mapp[i][j] === "#") {
                 empty = false;
             }
         }
 
         if (empty) {
             for (let i = 0; i < mapp.length; i++) {
-                mapp[i].splice(j, 0, ".");
+                mapp[i][j] = "*";
             }
-            j++;
         }
     }
 
-    console.log(mapp.map((val) => val.join("")));
+    // process.stdout.write(JSON.stringify(mapp.map((val) => val.length)) + "\n");
 
     console.log(calculateDistances(mapp));
 };
